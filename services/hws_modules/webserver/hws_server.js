13557e29954977b50721cf28a3cc7b5ab57561a7
Mohamed Mostafa <mhmd_mstafa90@yahoo.com>
2017-12-09 16:45:56 +0200

    modified hws_server for google instead of openshift

--- services/hws_modules/webserver/hws_server.js
+++ services/hws_modules/webserver/hws_server.js
@@ -344,11 +344,10 @@ var start = function () {
 
 	// routing - end
 
-	var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-	var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+	var port = process.env.PORT || 8081;
 
-	app.listen(port, ipaddress, function () {
-		console.log('%s: Node server started on %s:%d ...', Date(Date.now()), ipaddress, port);
+	app.listen(port, function () {
+		console.log('%s: Node server started on %s:%d ...', Date(Date.now()), port);
 	});
 };
 
